1cc90c688b8bb0d5f75399aab3eec6ce10ab3d27
ibnatysalsabila <salsajpr01@gmail.com>
2025-11-28 17:17:36 +0700

    membuat DestinasiEntry untuk navigasi halaman entry siswa

---
+++ app/src/main/java/com/example/myroomsatu_047/view/route/DestinasiEntry.kt
@@ -0,0 +1,10 @@
+package com.example.myroomsatu_047.view.route
+
+import com.example.myroomsatu_047.R
+import com.example.myroomsatu_047.view.route.DestinasiNavigasi
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+
+}
