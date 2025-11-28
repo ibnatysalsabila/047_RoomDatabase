cd1d813f37a0cd08a4d579c3dcc474db1d62429c
ibnatysalsabila <salsajpr01@gmail.com>
2025-11-28 18:07:13 +0700

    menambahkan fungsi untuk mengambil instance AplikasiSiswa dari CreationExtras

--- app/src/main/java/com/example/myroomsatu_047/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/myroomsatu_047/viewmodel/provider/PenyediaViewModel.kt
@@ -19,3 +19,6 @@ object PenyediaViewModel {
     }
 }
 
+fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory
+        .APPLICATION_KEY] as AplikasiSiswa)
