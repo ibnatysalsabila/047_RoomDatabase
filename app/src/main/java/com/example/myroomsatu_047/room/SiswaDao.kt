bbd7d9f6c9462184d5a2bc977846b33bc22a5caa
ibnatysalsabila <salsajpr01@gmail.com>
2025-11-28 15:22:19 +0700

    Membuat DAO untuk akses data Siswa dengan query mengambil seluruh data terurut nama

---
+++ app/src/main/java/com/example/myroomsatu_047/room/SiswaDao.kt
@@ -0,0 +1,14 @@
+package com.example.myroomsatu_047.room
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface SiswaDao {
+    @Query(value = "SELECT * from tblSiswa ORDER BY nama ASC")
+    fun getAllSiswa(): Flow<List<Siswa>>
+
+}
