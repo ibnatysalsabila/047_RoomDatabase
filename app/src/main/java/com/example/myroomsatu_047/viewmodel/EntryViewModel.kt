0245a3377db50fc638842d6fc248bc6941b51ccd
ibnatysalsabila <salsajpr01@gmail.com>
2025-11-28 18:09:23 +0700

    membuat ViewModel untuk mengelola input dan validasi data siswa

---
+++ app/src/main/java/com/example/myroomsatu_047/viewmodel/EntryViewModel.kt
@@ -0,0 +1,31 @@
+package com.example.myroomsatu_047.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.myroomsatu_047.repositori.RepositoriSiswa
+import com.example.myroomsatu_047.room.Siswa
+
+class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean{
+        return with(uiState){
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
+    fun updateUiState(detailSiswa: DetailSiswa){
+        uiStateSiswa =
+            UIStateSiswa(detailSiswa = detailSiswa,
+                isEntryValid = validasiInput(detailSiswa))
+    }
+
+    suspend fun saveSiswa(){
+        if (validasiInput()){
+            repositoriSiswa.insertSiswa(uiStateSiswa.detailSiswa.toSiswa())
+        }
+    }
+}
+
