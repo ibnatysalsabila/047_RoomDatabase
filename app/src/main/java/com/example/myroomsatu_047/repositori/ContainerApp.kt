9b92656a22b199ece58dbb77a02a4c751d291c8d
ibnatysalsabila <salsajpr01@gmail.com>
2025-11-28 15:51:57 +0700

    membuat container dan inisialisasi repository siswa di class Application

---
+++ app/src/main/java/com/example/myroomsatu_047/repositori/ContainerApp.kt
@@ -0,0 +1,28 @@
+package com.example.myroomsatu_047.repositori
+
+import android.app.Application
+import android.content.Context
+import com.example.myroomsatu_047.room.DatabaseSiswa
+
+interface ContainerApp{
+    val repositoriSiswa : RepositoriSiswa
+}
+
+class ContainerDataApp(private val context: Context):
+        ContainerApp {
+    override val repositoriSiswa: RepositoriSiswa by lazy {
+        OfflineRepositorSiswa(
+            siswaDao = DatabaseSiswa.getDatabase(context).siswaDao()
+        )
+
+    }
+}
+
+class AplikasiSiswa : Application () {
+    lateinit var container : ContainerDataApp
+
+    override fun onCreate() {
+        super.onCreate()
+        container = ContainerDataApp(context = this)
+    }
+}
