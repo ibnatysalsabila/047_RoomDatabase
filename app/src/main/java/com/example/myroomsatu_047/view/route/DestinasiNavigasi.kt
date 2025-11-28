47d19f6fd9773a1d404681c6c301895bd7c86ebc
ibnatysalsabila <salsajpr01@gmail.com>
2025-11-28 17:15:53 +0700

    membuat interface destinasi navigasi

---
+++ app/src/main/java/com/example/myroomsatu_047/view/route/DestinasiNavigasi.kt
@@ -0,0 +1,13 @@
+package com.example.myroomsatu_047.view.route
+
+interface DestinasiNavigasi {
+    /**
+     * Nama unik untuk menentukan jalur untuk composable
+     */
+    val route: String
+
+    /**
+     * String resource id yang berisi judul yang akan ditampilkan dilayar halaman.
+     */
+    val titleRes: Int
+}
