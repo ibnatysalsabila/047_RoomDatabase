1624c1e781f0b7905ba0cf4d2dd51597e1e03082
ibnatysalsabila <salsajpr01@gmail.com>
2025-11-28 18:31:08 +0700

    pemanggilan fungsi siswaapp pada main activity

--- app/src/main/java/com/example/myroomsatu_047/MainActivity.kt
+++ app/src/main/java/com/example/myroomsatu_047/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myroomsatu_047.ui.theme.MyRoomSatu_047Theme
+import com.example.myroomsatu_047.view.uicontroller.SiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyRoomSatu_047Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    SiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
