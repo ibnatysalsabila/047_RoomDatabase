54fb24f310aa883f06bfe178a84a55d775d57fc6
ibnatysalsabila <salsajpr01@gmail.com>
2025-11-28 15:43:34 +0700

    membuat repository untuk mengambil dan simpan data

--- app/src/main/java/com/example/myroomsatu_047/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/myroomsatu_047/repositori/RepositoriSiswa.kt
@@ -8,3 +8,10 @@ interface RepositoriSiswa{
     fun getAllSiswaStream(): Flow<List<Siswa>>
     suspend fun insertSiswa(siswa: Siswa)
 }
+
+class OfflineRepositorSiswa(
+    private val siswaDao: SiswaDao
+): RepositoriSiswa {
+    override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao .getAllSiswa()
+    override suspend fun insertSiswa(siswa: Siswa)  = siswaDao.insert(siswa)
+}
