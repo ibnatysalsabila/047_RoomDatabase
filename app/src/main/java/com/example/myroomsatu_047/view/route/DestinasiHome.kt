716ce83e7bcd10199bacdf6a5177c012eb9ec22f
ibnatysalsabila <salsajpr01@gmail.com>
2025-11-28 17:18:58 +0700

    menambahkan DestinasiHome sebagai destinasi navigasi halaman utama

---
+++ app/src/main/java/com/example/myroomsatu_047/view/route/DestinasiHome.kt
@@ -0,0 +1,9 @@
+package com.example.myroomsatu_047.view.route
+
+import com.example.myroomsatu_047.R
+import com.example.myroomsatu_047.view.route.DestinasiNavigasi
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
