9baa8e1accac70116d45dd8a3de51e54d7cf6504
ibnatysalsabila <salsajpr01@gmail.com>
2025-12-05 15:20:47 +0700

    menambahkan state detail siswa dan menampilkan isi bodynya

--- app/src/main/java/com/example/myroomsatu_047/view/HalamanDetailSiswa.kt
+++ app/src/main/java/com/example/myroomsatu_047/view/HalamanDetailSiswa.kt
@@ -74,3 +74,21 @@ fun DetailSiswaScreen(
         }, modifier = modifier
     ) { innerPadding ->
 
+        val uiState = viewModel.uiDetailState.collectAsState()
+        val coroutineScope = rememberCoroutineScope()
+
+        BodyDetailDataSiswa(
+            detailSiswaUiState = uiState.value,
+            onDelete = {
+                coroutineScope.launch {
+                    viewModel.deleteSiswa()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+        )
+    }
+}
+
