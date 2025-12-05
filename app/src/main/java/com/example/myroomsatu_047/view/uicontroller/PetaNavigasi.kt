e5c9310b9a76e7f2ad9b78779378e56abf8d9509
ibnatysalsabila <salsajpr01@gmail.com>
2025-12-05 16:44:08 +0700

    menambahkan navigate item update dan 2 composable route

--- app/src/main/java/com/example/myroomsatu_047/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/myroomsatu_047/view/uicontroller/PetaNavigasi.kt
@@ -4,11 +4,16 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.myroomsatu_047.view.DetailSiswaScreen
 import com.example.myroomsatu_047.view.EntrySiswaScreen
 import com.example.myroomsatu_047.view.HomeScreen
+import com.example.myroomsatu_047.view.route.DestinasiDetailSiswa
+import com.example.myroomsatu_047.view.route.DestinasiDetailSiswa.itemIdArg
 import com.example.myroomsatu_047.view.route.DestinasiEntry
 import com.example.myroomsatu_047.view.route.DestinasiHome
 
@@ -28,12 +33,26 @@ fun HostNavigasi(
     {
         composable(DestinasiHome.route){
             HomeScreen(
-                navigateToItemEntry = {navController.navigate(DestinasiEntry.route)},
+                navigateToItemEntry = {
+                    navController.navigate(DestinasiEntry.route)},
+
+                navigateToItemUpdate = {
+                    navController.navigate(route = "${DestinasiDetailSiswa.route}/${it}")}
             )
         }
         composable(DestinasiEntry.route){
             EntrySiswaScreen(navigateBack = { navController.popBackStack()})
         }
 
+        composable(route = DestinasiDetailSiswa.routeWithArgs,
+            arguments = listOf(navArgument(itemIdArg) {
+                type = NavType.IntType
+            })
+        ){
+            DetailSiswaScreen(
+                //navigateToEditItem = {navController.navigate("${DestinasiEditSiswa.route}/$it")},
+                navigateBack = { navController.navigateUp() })
+        }
     }
 }
+
