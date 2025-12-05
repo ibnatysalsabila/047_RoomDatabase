ee18c0b70cf92c67dc62d8f69ca4ae325a7e5acf
ibnatysalsabila <salsajpr01@gmail.com>
2025-12-05 15:51:49 +0700

    menambahkan fungsi untuk menghapus siswa dari database

--- app/src/main/java/com/example/myroomsatu_047/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/myroomsatu_047/viewmodel/DetailViewModel.kt
@@ -27,3 +27,12 @@ class DetailViewModel (
                 started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
                 initialValue = DetailSiswaUiState()
             )
+    suspend fun deleteSiswa(){
+        repositoriSiswa.deleteSiswa(uiDetailState.value.detailSiswa.toSiswa())
+    }
+
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+}
+
